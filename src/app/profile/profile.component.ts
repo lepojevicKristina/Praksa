135474d22d20989e5a131f74ce4a37d1b5b02817
lepojevicKristina <lepojevickristina7@gmail.com>
2019-08-07 15:19:10 +0200

    Upload image

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { AuthenticationService } from '../authentication/authentication.service';
 import { DomSanitizer } from '@angular/platform-browser';
+import { HttpMethod } from 'blocking-proxy/built/lib/webdriver_commands';
+import { GlobalConstantService } from '../global-constant.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-profile',
@@ -14,6 +17,7 @@ export class ProfileComponent implements OnInit
   imageUrl: string = "/assets/img/default_image.jpg";
   selectedImage: File = null;
   fileToUpload: File = null;
+  uploadForm: FormGroup;
 
   fullname: string = 'undefined';
   email: string = 'undefined';
@@ -22,7 +26,9 @@ export class ProfileComponent implements OnInit
 
   constructor(private formBuilder: FormBuilder, 
               private authenticationService: AuthenticationService, 
-              private sanitizer: DomSanitizer) 
+              private sanitizer: DomSanitizer,
+              private http: HttpClient,
+              private global: GlobalConstantService) 
   { }
 
   ngOnInit() 
@@ -39,40 +45,49 @@ export class ProfileComponent implements OnInit
         }
       )
 
+      this.uploadForm = this.formBuilder.group({
+        avatar: ['']
+      });
   }
 
   
 
   onSubmit() 
   {
-    //console.log(this.fileToUpload);
-   // let formData 
+    console.log(this.fileToUpload);
+   //const formData = new FormData();
+   //formData.append('file', this.fileToUpload, this.fileToUpload.name);
 
-   const formData = new FormData();
-   formData.append('file', this.fileToUpload, this.fileToUpload.name);
+    //console.log("form data  " + formData);
 
-   const uploadData = new FormData();
-    uploadData.append('myFile', this.selectedImage, this.selectedImage.name);
+    let formData: FormData = new FormData();
+    //formData.append('avatar', this.fileToUpload, this.fileToUpload.name);
+    //formData.append('s', JSON.stringify("s"));
 
+    const token = localStorage.getItem('token');
 
-    console.log("form data  " + formData);
+    formData.append('file', this.uploadForm.get('avatar').value);
+    formData.append('token', token);
 
-    this.authenticationService.uploadImage(formData)
+    console.log("formdata   " + formData);
+
+    //this.authenticationService.uploadImage(formData)
+   //this.authenticationService.uploadImage(this.fileToUpload)
+   this.http.post<any>(this.global.SERVER_URL + '/api/image', {formData})
       .subscribe(
         response =>
         {
-          console.log(response);
+          console.log(response + " update response");
         }
       )
 
   }
 
-  imageToUpload(file: FileList)
+  imageToUpload(files: FileList)
   {
-    
 
-    this.selectedImage = file.item(0);
-    this.fileToUpload = file.item(0);
+    this.selectedImage = files.item(0);
+    this.fileToUpload = files.item(0);
 
     var reader = new FileReader();
     reader.onload = (event: any) => 
@@ -80,8 +95,19 @@ export class ProfileComponent implements OnInit
       this.imageUrl = event.target.result;
     }
     reader.readAsDataURL(this.selectedImage);
+    //console.log(this.selectedImage);
+    const token = localStorage.getItem('token');
 
-    console.log(this.selectedImage);
+    this.fileToUpload = files.item(0); 
+    let formData = new FormData(); 
+    formData.append('file', this.fileToUpload, this.fileToUpload.name);
+    formData.append('token', token); 
+    this.http.post(this.global.SERVER_URL + '/api/image', formData)
+      .subscribe((val) => {
+
+      console.log(val);
+      });
+      return false; 
 
   }
 
